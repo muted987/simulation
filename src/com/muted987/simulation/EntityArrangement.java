d19f615f802f3fff0d5ff0a2937e11a59d6231d0
SunSetS5S <akashi322wp@yandex.ru>
2025-01-27 17:53:35 +0300

    feat: новый класс
    класс для дефолтного расположения на SimulationMap

---
+++ src/com/muted987/simulation/EntityArrangement.java
@@ -0,0 +1,29 @@
+package com.muted987.simulation;
+
+import com.muted987.simulation.entity.*;
+import com.muted987.simulation.entity.creature.Herbivore;
+import com.muted987.simulation.entity.creature.Predator;
+import com.muted987.simulation.simulationMap.SimulationMap;
+
+public class EntityArrangement {
+    public SimulationMap defaultArrangement() {
+        Coordinates herbivoreCoordinates = new Coordinates(1, 3);
+        Coordinates predatorCoordinates = new Coordinates(10, 8);
+        Coordinates firstGrassCoordinates = new Coordinates(7, 6);
+        Coordinates secondGrassCoordinates = new Coordinates(6, 8);
+        Coordinates firstRockCoordinates = new Coordinates(3, 9);
+        Coordinates secondRockCoordinates = new Coordinates(5, 7);
+        Coordinates firstTreeCoordinates = new Coordinates(9, 6);
+        Coordinates secondTreeCoordinates = new Coordinates(4, 3);
+        SimulationMap simulationMap = new SimulationMap();
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Herbivore, herbivoreCoordinates), herbivoreCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Predator, predatorCoordinates), predatorCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Grass, firstGrassCoordinates), firstGrassCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Grass, secondGrassCoordinates), secondGrassCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Rock, firstRockCoordinates), firstRockCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Rock, secondRockCoordinates), secondRockCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Tree, firstTreeCoordinates), firstTreeCoordinates);
+        simulationMap.setEntity(EntityFactory.createEntity(EntitySymbol.Tree, secondTreeCoordinates), secondTreeCoordinates);
+        return simulationMap;
+    }
+}
