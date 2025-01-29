83b03db3e8dc4eedb18d792d71c3dbca7fe79c3f
SunSetS5S <akashi322wp@yandex.ru>
2025-01-29 17:48:29 +0300

    ничего не изменилось. класс используется для теста новых классов и функций

--- src/com/muted987/simulation/Main.java
+++ src/com/muted987/simulation/Main.java
@@ -1,10 +1,12 @@
 package com.muted987.simulation;
 
 import com.muted987.simulation.simulationMap.SimulationMap;
+import com.muted987.simulation.simulationMap.arrangement.DefaultArrangement;
+import com.muted987.simulation.simulationMap.arrangement.RandomArrangement;
 
 public class Main {
     public static void main(String[] args) {
-        SimulationMap simulationMap = (new EntityArrangement()).defaultArrangement();
+        SimulationMap simulationMap = (new RandomArrangement()).arrange();
         ConsoleRender render = new ConsoleRender();
         render.render(simulationMap);
 
