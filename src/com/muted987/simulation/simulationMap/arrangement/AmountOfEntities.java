af9744879b7585cdf4bec7962e7c70a3240ff96a
SunSetS5S <akashi322wp@yandex.ru>
2025-01-29 17:47:33 +0300

    feat: новый класс определяющий количество сущностей

---
+++ src/com/muted987/simulation/simulationMap/arrangement/AmountOfEntities.java
@@ -0,0 +1,22 @@
+package com.muted987.simulation.simulationMap.arrangement;
+
+import com.muted987.simulation.entity.EntitySymbol;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AmountOfEntities {
+    private final Map<EntitySymbol, Integer> amountOfEntities = new HashMap<>();
+
+    {
+        amountOfEntities.put(EntitySymbol.Grass, 2);
+        amountOfEntities.put(EntitySymbol.Rock, 2);
+        amountOfEntities.put(EntitySymbol.Tree, 2);
+        amountOfEntities.put(EntitySymbol.Herbivore, 1);
+        amountOfEntities.put(EntitySymbol.Predator, 1);
+    }
+
+    public Map<EntitySymbol, Integer> getAmountOfEntities() {
+        return amountOfEntities;
+    }
+}
